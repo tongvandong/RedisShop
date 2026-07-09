7c83f8dfa32bd4dd362044b88386ddf32a650ea9
taoladong <taoladong145@gmail.com>
2026-07-09 15:55:33 +0700

    Add click to component Vite plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { reactClickToComponent } from 'vite-plugin-react-click-to-component'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    reactClickToComponent(),
+  ],
 })
--- redis-dashboard/vite.config.js
+++ redis-dashboard/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { reactClickToComponent } from 'vite-plugin-react-click-to-component'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    reactClickToComponent(),
+  ],
 })
